732026053eb09c21b2978333574d6fe5dbbb8957
korpg95273 <korpg95273@gmail.com>
2024-03-18 10:30:11 +0900

    C글쓰기 주석/빌더패턴

--- src/main/java/org/iclass/vo/Notice.java
+++ src/main/java/org/iclass/vo/Notice.java
@@ -23,3 +23,9 @@ public class Notice {
 	private String uploadFile;
 
 }
+/*
+	Builder 애노테이션은 빌더패턴이라는 형식으로 객체 생성과 초기화를 합니다.
+	- 생성자로 객체 생성과 모든 프로퍼티 변수를 초기화 하였던 형식을 대신하여
+	- 객체 생성 메소드와 각각의 프로퍼티 변수를 초기화 하는 메소드가 정의되어 있습니다.
+	- NoticeWriteSaveController 의 49번 라인에서 활용하였습니다.
+*/
